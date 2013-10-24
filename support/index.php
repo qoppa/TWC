7b5599446cf4881a2479c1f8e7af2200543d1719
qoppa <max.quattromani@twcable.com>
2013-10-23 22:17:41 -0600

    test removed

--- support/index.php
+++ support/index.php
@@ -1,6 +1,5 @@
 <?php include($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/header.php'); ?>
 
 			<!-- Build Page Here -->
-			test
 
 <?php include($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/footer.php'); ?>
