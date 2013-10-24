32ec2b3646a50bff577b576f9001bc02b5ce73a4
qoppa <max.quattromani@twcable.com>
2013-10-24 16:50:53 -0600

    updating filter menu for stores

--- support/stores/locator/soCal/index.php
+++ support/stores/locator/soCal/index.php
@@ -2,6 +2,156 @@
 		
         <p class="subHead h6">Enter the ZIP Code or location of your service address below.</p>
         
+        <!-- TEST SECTION -->
+        
+        <div class="locate-stores">
+			<form action="searchPage">		
+
+			<div class="50-50 section columnControl">
+				<div class="parsys_column twc-col2_5050">
+					<div class="parsys parsys0 twc-col2_5050-c0 parsys_column">
+						<div class="parbase section">
+							
+							<p class="light">Is this the ZIP Code for your service address?<br />If not, enter it now.</p>
+							
+						</div>
+						<div class="new section"></div>
+					</div>
+					<div class="parsys parsys1 twc-col2_5050-c1 parsys_column">
+						<div class="parbase section">
+							
+							<input id="" type="search" placeholder="90064" name="">
+							<small>Enter ZIP Codes, or Street Address, City, State</small>
+						</div>
+						<div class="new section"></div>
+					</div>
+				</div>
+				<div class="columnClear"></div>
+			</div>
+
+				<div class="33-67 section columnControl">
+					<div class="parsys_column twc-col2_3367">
+						
+						<div class="parsys parsys0 twc-col2_3367-c0 parsys_column">
+							<div class="parbase section">		
+								
+								<!-- locate-stores input -->
+								<div class="stores-find-me">
+									<div class="twc-module">
+										<fieldset class="twc-search">
+											<div class="twc-search-input-wrap">
+												<p class="light">Is this the ZIP Code for your service address?<br />If not, enter it now.</p>
+												<input id="" type="search" placeholder="90064" name="">
+												<div class="cta omega">
+                                                    <a href="#" analyticsname="test">
+                                                        <span>GO</span>
+                                                    </a>
+                                                </div>											
+												<small>Enter ZIP Codes, or Street Address, City, State</small>
+											</div>
+                                            
+                                            <div class="stores_details_map_info_distance twc-left">
+                                                <span> WITHIN:</span>
+                                                <select name="distance">
+                                                    <option>40 Miles
+                                                    <option>25 Miles
+                                                    <option>10 Miles
+                                                    <option>5 Miles
+                                                </select>
+                                            </div>
+										</fieldset>
+									</div>
+								</div>
+								<!-- .locate-stores input -->
+							</div>
+						</div>
+					
+						<div class="parsys parsys1 twc-col2_3367-c1 parsys_column">
+							<div class="parbase section">
+								
+								<!-- locate stores filters -->
+								<div class="locate-stores-filters">
+									<fieldset>
+										<legend class="light">Uncheck items below to remove from results</legend>
+										<div class="filter-section">
+											<div class="mobile-filters-options">
+											    <div class="btn blue twc-left">
+    											    <button type="submit">Apply Filters</button>
+											    </div>
+												<div class="btn blue twc-right">
+    												<button type="clear">Reset Filters</button>
+												</div>
+											</div>
+											
+											<div class="25-50-25 section columnControl">
+												<div class="parsys_column twc-col3_255025">
+													<div class="parsys parsys0 twc-col3_255025-c0 parsys_column">
+														<div class="parbase section filter-section-payment">
+															<label for="payment">Payment Types</label>
+															<ul class="filter-checkbox">
+																<li><input id="icon-money" type="checkbox" checked name="" id=""><label>Cash<label></li>
+																<li><input id="icon-list-alt" type="checkbox" checked name="" id=""><label>Check</label></li>
+																<li><input id="icon-credit-card" type="checkbox" checked name="" id=""><label>Credit Card</label></li>
+															</ul>
+														</div>	
+													</div>
+													
+													<div class="parsys parsys1 twc-col3_255025-c1 parsys_column">
+														<div class="parbase section filter-section-services">
+															<label for="services">Services Available</label>
+                                                    			<div class="50-50 section columnControl">
+                                                    				<div class="parsys_column twc-col2_5050">
+                                                    					<div class="parsys parsys0 twc-col2_5050-c0 parsys_column">
+                                                    						<div class="parbase section">
+                                                                                <ul>
+                                                                                    <li><input id="icon-eye-open" type="checkbox" checked name="" id=""><label>Demo Center</label></li>
+                                                    								<li><input id="icon-wrench" type="checkbox" checked name="" id=""><label>Self Install</label></li>
+                                                    								<li><input id="icon-dollar" type="checkbox" checked name="" id=""><label>Payment Centers</label></li>
+                                                                                </ul>
+                                                    						</div>
+                                                    						<div class="new section"></div>
+                                                    					</div>
+                                                    					<div class="parsys parsys1 twc-col2_5050-c1 parsys_column">
+                                                    						<div class="parbase section">
+                                                                                <ul>
+                                                                                    <li><input id="icon-time" type="checkbox" checked name="" id=""><label>24-Hour Payment</label></li>
+                                                    								<li><input id="icon-download" type="checkbox" checked name="" id=""><label>Return Equipment</label></li>
+                                                    								<li><input id="icon-exchange" type="checkbox" checked name=""><label>Exchange Equipment</label></li>
+                                                                                </ul>
+                                                    						</div>
+                                                    						<div class="new section"></div>
+                                                    					</div>
+                                                    				</div>
+                                                    				<div class="columnClear"></div>
+                                                    			</div>
+														</div>
+													</div>	
+													
+													<div class="parsys parsys0 twc-col3_255025-c0 parsys_column">
+														<div class="parbase section filter-section-office">
+															<label for="office">Office</label>
+															<ul>
+																<li><input id="icon-building" type="checkbox" checked name="" id=""><label>TWC Office Only</label></li>
+																<li><input id="icon-shopping-cart" type="checkbox" checked name="" id=""><label>Retail Stores</label></li>
+															</ul>
+														</div>
+													</div>	
+												</div>
+											</div>
+										</div>
+									</fieldset>
+
+								</div>
+								<!-- .locate stores filters -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>   
+        
+        <!-- .TEST SECTION -->
+        
 		<div class="locate-stores">
 			<form action="searchPage">		
 
