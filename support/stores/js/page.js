4f98adfedeab6ba3a8c442fbbc3617e8c7c437cb
qoppa <max.quattromani@twcable.com>
2013-11-04 16:39:10 -0700

    updated code for stores page and general global updates

--- support/stores/js/page.js
+++ support/stores/js/page.js
@@ -1,10 +1,7 @@
 // Place page specific jQuery here
-$(".close").click(function() {
-	$('video')[0].pause();
-});
-
 
 $('.refineList a').click(function(){
+    $('#before-filters .cta').addClass('disabled');
 	$('.form-filters').toggle();
 	
 	if($('.form-filters').is(':hidden')){
